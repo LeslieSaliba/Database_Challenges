394bf6b63677f297f3314c446cab141bd608439e
Leslie Saliba <lesliesaliba@gmail.com>
2023-09-14 18:09:41 +0300

    MongoDB - Done with challenge 4

--- NoSQL-MongoDB/app.js
+++ NoSQL-MongoDB/app.js
@@ -43,3 +43,88 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+// Mongoose
+
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB!");
+});
+
+// Define a User schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+});
+
+// Create a User model from the schema
+const User = mongoose.model('User', userSchema);
+
+// Create a new user
+app.post("/api/users", async (req, res) => {
+  try {
+    const { username, email } = req.body;
+    if (!username || !email) {
+      return res.status(400).json({ error: "Username and email are required fields" });
+    }
+    const newUser = new User({ username, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Failed to create user" });
+  }
+});
+
+
+// Get all users
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
+
+// Update a user by ID
+app.put("/api/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, email } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(id, { username, email }, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(updatedUser);
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ error: "Failed to update user" });
+  }
+});
+
+// Delete a user by ID
+app.delete("/api/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(deletedUser);
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ error: "Failed to delete user" });
+  }
+});

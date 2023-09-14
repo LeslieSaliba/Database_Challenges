349530fae1fb2f304bd87453923ebe108eade37a
Leslie Saliba <lesliesaliba@gmail.com>
2023-09-14 10:40:03 +0300

    MongoDB - Done with challenge 2

---
+++ NoSQL-MongoDB/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const cors = require("cors");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  console.log(`Listening at http://localhost:${port}`);
+  res.send("This is working!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
